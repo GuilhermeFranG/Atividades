011516ee552d46701e46a0c26440393df1ff8ff1
Aprendiz TI <qhilherme@gmail.com>
2024-01-23 17:22:42 -0300

    Novo script - Botão de Descrição.

--- Atividade_6/AT6.js
+++ Atividade_6/AT6.js
@@ -22,15 +22,35 @@ function showSlides(n) {
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("demo");
     var captionText = document.getElementById("caption");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    var descriptionText = document.getElementById("imageDescription");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
+
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
     captionText.innerHTML = dots[slideIndex - 1].alt;
+    
+    var description = dots[slideIndex - 1].getAttribute('data-description');
+    if (description && description.trim() !== "") {
+        descriptionText.innerHTML = description;
+        descriptionText.classList.add('hidden');
+    } else {
+        descriptionText.classList.remove('hidden');
+    }
+}
+
+function toggleDescription() {
+    const descriptionElement = document.getElementById('imageDescription');
+    descriptionElement.classList.toggle('hidden');
 }
